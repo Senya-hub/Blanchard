d1ec011963a24453b7454c94d30bdbeb48770d51
Senya-hub <78022528+Senya-hub@users.noreply.github.com>
2021-03-28 14:52:59 +0900

    add header v1

--- script/index.js
+++ script/index.js
@@ -1,18 +1,33 @@
-$( document ).ready(function(){
-    $('#header-search').on('click',function(){
-        $('#menu-scan').toggle('.is-active');
-    });
-    $('#scan-close').click(function(){
-        $('#menu-scan').toggle('.is-active');
-    });
+window.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('#burger').addEventListener('click', function() {
+       document.querySelector('#menu-burger').classList.toggle('is-active')
+    })
+    document.querySelector('#burger-close').addEventListener('click', function() {
+        document.querySelector('#menu-burger').classList.toggle('is-active')
+     })
 
-    $('#burger').on('click',function(){
-        $('#menu-burger').toggle('.is-active');
-    });
-    $('#burger-close').click(function(){
-        $('#menu-burger').toggle('.is-active');
-    });
-    $('#scan-search').click(function(){
-        $('#menu-scan').hide('.header-scan');
-    });
+     document.querySelector('#header-search').addEventListener('click', function() {
+        document.querySelector('#menu-scan').classList.toggle('is-activ')
+     })
+     document.querySelector('#scan-close').addEventListener('click', function() {
+        document.querySelector('#menu-scan').classList.toggle('is-activ')
+     })
+})
+
+$( document ).ready(function(){
+    
+    
+    // $('#header-search').on('click',function(){
+    //     $('#menu-scan').toggle('.is-active');
+    // });
+    //  $('#scan-close').click(function(){
+    //      $('#menu-scan').toggle('.is-active');
+    //  });
+
+    
+     
+    //  $('#scan-search').click(function(){
+    //      $('#menu-scan').hide('.header-scan');
+    //  });
 });
+ 
